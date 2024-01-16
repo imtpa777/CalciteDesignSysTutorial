f3324ad385d74bc0545af8031908215dc168796a
Imtiyaz Pasha <imtiyazpasha@gmail.com>
2024-01-16 23:08:38 +0300

    16012024

---
+++ app.js
@@ -0,0 +1,13 @@
+require([
+    "esri/WebMap",
+    "esri/views/MapView",
+    "esri/widgets/Bookmarks",
+    "esri/widgets/BasemapGallery",
+    "esri/widgets/LayerList",
+    "esri/widgets/Legend",
+    "esri/widgets/Print",
+    "esri/config"
+], 
+    function(WebMap, MapView, Bookmarks, BasemapGallery, LayerList, Legend, Print, esriConfig){
+        esriConfig.apiKey = "";
+    });
