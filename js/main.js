1fd221e509faa93e4b785b60957952c9151fb775
Imtiyaz Pasha <imtiyazpasha@gmail.com>
2024-02-17 12:13:43 +0300

    main

--- js/main.js
+++ js/main.js
@@ -1,15 +1,5 @@
-/* require([
-    "esri/WebMap",
-    "esri/views/MapView",
-    "esri/widgets/Bookmarks",
-    "esri/widgets/BasemapGallery",
-    "esri/widgets/LayerList",
-    "esri/widgets/Legend",
-    "esri/widgets/Print"
-  ], function(WebMap, MapView, Bookmarks, BasemapGallery, LayerList, Legend, Print) { */
-
 import esriConfig from "https://js.arcgis.com/4.27/@arcgis/core/config.js";
-/* import Map from "https://js.arcgis.com/4.27/@arcgis/core/Map.js"; */
+import Map from "https://js.arcgis.com/4.27/@arcgis/core/Map.js"; 
 import WebMap from "https://js.arcgis.com/4.27/@arcgis/core/WebMap.js";
 import MapView from "https://js.arcgis.com/4.27/@arcgis/core/views/MapView.js";
 import Bookmarks from "https://js.arcgis.com/4.27/@arcgis/core/widgets/Bookmarks.js";
@@ -19,8 +9,7 @@ import Print from "https://js.arcgis.com/4.27/@arcgis/core/widgets/Print.js";
 import Home from "https://js.arcgis.com/4.27/@arcgis/core/widgets/Home.js";
 import Locate from "https://js.arcgis.com/4.27/@arcgis/core/widgets/Locate.js";
 import BasemapGallery from "https://js.arcgis.com/4.27/@arcgis/core/widgets/BasemapGallery.js";
-import appConfig from "./assets/config.js"
-
+import appConfig from "../config/config.js"
     const webmapId = new URLSearchParams(window.location.search).get("webmap") ?? "210c5b77056846808c7a5ce93920be81";
 
     const map = new WebMap({
@@ -28,7 +17,7 @@ import appConfig from "./assets/config.js"
         id: webmapId
       }
     });
-    console.log(appConfig.portalUrl);
+
     const view = new MapView({
       map,
       container: "viewDiv",
@@ -37,29 +26,29 @@ import appConfig from "./assets/config.js"
       }
     });
 
-    view.ui.move("zoom", "bottom-right");
-
-    const locateWidget = new Locate({ view });
-    const homeWidget = new Home({ view });
-    view.ui.add([homeWidget, locateWidget], "bottom-right");
+    view.ui.move("zoom", "top-left");
 
     const basemaps = new BasemapGallery({
       view,
       container: "basemaps-container"
     });
+
     const bookmarks = new Bookmarks({
       view,
       container: "bookmarks-container"
     });
+
     const layerList = new LayerList({
       view,
       selectionEnabled: true,
       container: "layers-container"
     });
+
     const legend = new Legend({
       view,
       container: "legend-container"
     });
+
     const print = new Print({
       view,
       container: "print-container"
@@ -67,14 +56,12 @@ import appConfig from "./assets/config.js"
 
     map.when(() => {
       const { title, description, thumbnailUrl, avgRating } = map.portalItem;
-      document.querySelector("#header-title").textContent = title;
+      //document.querySelector("#header-title").textContent = title;
       document.querySelector("#item-description").innerHTML = description;
       document.querySelector("#item-thumbnail").src = thumbnailUrl;
       document.querySelector("#item-rating").value = avgRating;
 
-    });
-
-    let activeWidget;
+      let activeWidget;
 
       const handleActionBarClick = ({ target }) => {
         if (target.tagName !== "CALCITE-ACTION") {
@@ -97,4 +84,18 @@ import appConfig from "./assets/config.js"
       };
 
       document.querySelector("calcite-action-bar").addEventListener("click", handleActionBarClick);
+
+      let actionBarExpanded = false;
+
+      document.addEventListener("calciteActionBarToggle", event => {
+        actionBarExpanded = !actionBarExpanded;
+        view.padding = {
+          left: actionBarExpanded ? 150 : 49
+        };
+      });
+
+      document.querySelector("calcite-shell").hidden = false;
+      document.querySelector("calcite-loader").hidden = true;
+
+    });
 /*   }); */
